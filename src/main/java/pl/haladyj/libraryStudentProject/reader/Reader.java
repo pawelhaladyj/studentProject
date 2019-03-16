0c2bf81613d53d3737f1eb526fae94f6f57336eb
Pawel Haladyj <pawel@haladyj.pl>
2019-03-16 17:09:15 +0100

    mvn version update, Reader.class - @

--- src/main/java/pl/haladyj/libraryStudentProject/reader/Reader.java
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/Reader.java
@@ -1,12 +1,31 @@
 package pl.haladyj.libraryStudentProject.reader;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+//@Entity
+@Data
+@Table(name ="reader")
 public class Reader {
 
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column(name = "id")
     private Long id;
+
+    //@Column(name ="firstName")
+    //@NotNull
     private String firstName;
+
+    //@Column(name = "lastName")
+    //@NotNull
     private String lastName;
+
+    //@Column(name = "ssn")
     private Long ssn;
+
     private List<String> contact;
 }
