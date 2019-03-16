d16506849327fa8ee029342fbaca8bed5bf8f2fa
Pawel Haladyj <pawel@haladyj.pl>
2019-03-16 16:15:07 +0100

    Reader.class init

---
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/Reader.java
@@ -0,0 +1,12 @@
+package pl.haladyj.libraryStudentProject.reader;
+
+import java.util.List;
+
+public class Reader {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Long ssn;
+    private List<String> contact;
+}
