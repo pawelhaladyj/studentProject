f17fb544640c16b80f68766a6e9244396ce1334e
Pawel Haladyj <pawel@haladyj.pl>
2019-03-19 18:47:15 +0100

    ReaderService, ReaderRepository init

---
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/ReaderRepository.java
@@ -0,0 +1,7 @@
+package pl.haladyj.libraryStudentProject.reader;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
+
+}
---
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/ReaderService.java
@@ -0,0 +1,14 @@
+package pl.haladyj.libraryStudentProject.reader;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReaderService {
+
+    private final ReaderRepository readerRepository;
+
+
+    public ReaderService(ReaderRepository readerRepository) {
+        this.readerRepository = readerRepository;
+    }
+}
