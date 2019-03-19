0841338c539fea925f1ed7d007da88f33c49f7b7
Pawel Haladyj <pawel@haladyj.pl>
2019-03-19 18:42:09 +0100

    fixed Reader.class

--- src/main/java/pl/haladyj/libraryStudentProject/reader/Reader.java
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/Reader.java
@@ -3,6 +3,7 @@ package pl.haladyj.libraryStudentProject.reader;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -13,5 +14,16 @@ public class Reader {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "first_name")
+    @NotNull
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotNull
+    private String lastName;
+
+    @Column(name = "ssn")
+    private Long ssn;
 
 }
