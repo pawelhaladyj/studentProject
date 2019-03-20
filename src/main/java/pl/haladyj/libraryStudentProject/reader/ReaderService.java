5cb80cec55d2c6bce18000076a93526d41113df1
Pawel Haladyj <pawel@haladyj.pl>
2019-03-20 17:48:12 +0100

    ReaderService::findAllReaders

--- src/main/java/pl/haladyj/libraryStudentProject/reader/ReaderService.java
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/ReaderService.java
@@ -3,7 +3,9 @@ package pl.haladyj.libraryStudentProject.reader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ReaderService {
@@ -18,5 +20,11 @@ public class ReaderService {
     public Optional<ReaderDto> findReaderById(long readerId){
         return readerRepository.findById(readerId).map(ReaderConverter::toDto);
     }
+
+    public List<ReaderDto> findAllReaders(){
+        return readerRepository.findAll().stream().map(ReaderConverter::toDto).collect(Collectors.toList());
+    }
+
+
 
 }
