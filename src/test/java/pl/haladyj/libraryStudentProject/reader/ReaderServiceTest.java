a5dbee6685964b21e94359179eb66a278932ac31
Pawel Haladyj <pawel@haladyj.pl>
2019-03-22 16:25:08 +0100

    ReaderServiceTest init

---
+++ src/test/java/pl/haladyj/libraryStudentProject/reader/ReaderServiceTest.java
@@ -0,0 +1,38 @@
+package pl.haladyj.libraryStudentProject.reader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReaderServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findReaderById() {
+    }
+
+    @Test
+    public void findAllReaders() {
+    }
+
+    @Test
+    public void createReader() {
+    }
+
+    @Test
+    public void updateReader() {
+    }
+
+    @Test
+    public void deleteReaderById() {
+    }
+}
