72f857fe5a7b87c0ca65257ae1a7e6e00c23649d
Pawel Haladyj <pawel@haladyj.pl>
2019-03-26 18:40:29 +0100

    entityManagerBug

---
+++ src/main/java/pl/haladyj/libraryStudentProject/author/Author.java
@@ -0,0 +1,28 @@
+package pl.haladyj.libraryStudentProject.author;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+//@Entity
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName")
+    @NotEmpty
+    private String firstName;
+
+    @Column(name = "middleName")
+    private String middleName;
+
+    @Column(name = "lastName")
+    @NotEmpty
+    private String lastName;
+}
