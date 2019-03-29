851539142dad4c6f41c5b0941e79d77784faf4a0
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 21:41:47 +0100

    AuthorService AuthorRepository init

---
+++ src/main/java/pl/haladyj/libraryStudentProject/author/AuthorRepository.java
@@ -0,0 +1,9 @@
+package pl.haladyj.libraryStudentProject.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
---
+++ src/main/java/pl/haladyj/libraryStudentProject/author/AuthorService.java
@@ -0,0 +1,13 @@
+package pl.haladyj.libraryStudentProject.author;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+}
