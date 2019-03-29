ee1dc1bcf3e741c4d66ee8dfb4915bda622a268b
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 16:58:49 +0100

    fixed BookRepository

--- src/main/java/pl/haladyj/libraryStudentProject/book/BookRepository.java
+++ src/main/java/pl/haladyj/libraryStudentProject/book/BookRepository.java
@@ -2,7 +2,7 @@ package pl.haladyj.libraryStudentProject.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Long, Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 
 }
