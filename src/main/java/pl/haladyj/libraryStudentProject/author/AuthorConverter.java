793e8820ac1988d12f7a457c0e0eaf18fb6267a4
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 21:47:22 +0100

    AuthorConverter init

---
+++ src/main/java/pl/haladyj/libraryStudentProject/author/AuthorConverter.java
@@ -0,0 +1,16 @@
+package pl.haladyj.libraryStudentProject.author;
+
+import pl.haladyj.libraryStudentProject.utils.Converter;
+
+public class AuthorConverter implements Converter<Author, AuthorDto> {
+
+    @Override
+    public Author toEntity(AuthorDto authorDto) {
+        return null;
+    }
+
+    @Override
+    public AuthorDto toDto(Author author) {
+        return null;
+    }
+}
