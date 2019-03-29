9dc95b132a5fafd991596b250154d1dced2432d8
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 16:55:33 +0100

    BookRepository init

---
+++ src/main/java/pl/haladyj/libraryStudentProject/book/BookRepository.java
@@ -0,0 +1,8 @@
+package pl.haladyj.libraryStudentProject.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Long, Book> {
+
+
+}
--- src/main/java/pl/haladyj/libraryStudentProject/book/BookService.java
+++ src/main/java/pl/haladyj/libraryStudentProject/book/BookService.java
@@ -1,4 +1,9 @@
 package pl.haladyj.libraryStudentProject.book;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
 }
