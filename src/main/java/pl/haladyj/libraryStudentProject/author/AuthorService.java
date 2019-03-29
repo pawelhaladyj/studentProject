6351816f4b0ff2839ba60ef058afad9b72303c1f
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 22:01:27 +0100

    AuthorService CRUD init

--- src/main/java/pl/haladyj/libraryStudentProject/author/AuthorService.java
+++ src/main/java/pl/haladyj/libraryStudentProject/author/AuthorService.java
@@ -2,6 +2,9 @@ package pl.haladyj.libraryStudentProject.author;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AuthorService {
 
@@ -11,5 +14,23 @@ public class AuthorService {
         this.authorRepository = authorRepository;
     }
 
+    public AuthorDto findById(Long id){
+        return null;
+    }
 
+    public List<AuthorDto> findAll(){
+        return new ArrayList<>();
+    }
+
+    public AuthorDto createAuthor(AuthorDto authorDto){
+        return null;
+    }
+
+    public AuthorDto updateAuthor(AuthorDto authorDto){
+        return null;
+    }
+
+    public void deleteAuthor (AuthorDto authorDto){
+
+    }
 }
