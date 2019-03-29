10e3a8d9f8baf72f0553153f4af1bcbc91e0a277
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 20:14:10 +0100

    fixed BookConverter

--- src/main/java/pl/haladyj/libraryStudentProject/book/BookConverter.java
+++ src/main/java/pl/haladyj/libraryStudentProject/book/BookConverter.java
@@ -2,10 +2,13 @@ package pl.haladyj.libraryStudentProject.book;
 
 import pl.haladyj.libraryStudentProject.utils.Converter;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class BookConverter implements Converter <Book, BookDto> {
 
     @Override
     public Book toEntity(BookDto bookDto) {
+        checkNotNull(bookDto, "Expected not null DTO");
         Book book = new Book();
         book.setId(bookDto.getId());
         book.setTitle(bookDto.getTitle());
@@ -17,6 +20,7 @@ public class BookConverter implements Converter <Book, BookDto> {
 
     @Override
     public BookDto toDto(Book book) {
+        checkNotNull(book, "Expected not null Entity");
         BookDto bookDto = new BookDto();
         bookDto.setId(book.getId());
         bookDto.setTitle(book.getTitle());
