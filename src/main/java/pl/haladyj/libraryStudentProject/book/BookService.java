de1f2c9dfb2536856201fe22b683bb675ad5a991
Pawel Haladyj <pawel@haladyj.pl>
2019-03-29 17:11:47 +0100

    BookService CRUD

--- src/main/java/pl/haladyj/libraryStudentProject/book/BookService.java
+++ src/main/java/pl/haladyj/libraryStudentProject/book/BookService.java
@@ -3,6 +3,9 @@ package pl.haladyj.libraryStudentProject.book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BookService {
 
@@ -12,4 +15,26 @@ public class BookService {
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
+
+    public Book findById (Long id){
+        return new Book();
+    }
+
+    public List<Book> findAll(){
+        return new ArrayList<>();
+    }
+
+    public Book create(Book book){
+        return bookRepository.save(book);
+    }
+
+    public Book update(Book book){
+        return bookRepository.save(book);
+    }
+
+    public void delete(Book book){
+        bookRepository.delete(book);
+    }
+
+
 }
