057d9ee86f522fb5984922f9715d4a7d65f213f0
Pawel Haladyj <pawel@haladyj.pl>
2019-04-14 18:17:54 +0200

    ReaderController findById, findAll, create

--- src/main/java/pl/haladyj/libraryStudentProject/reader/ReaderController.java
+++ src/main/java/pl/haladyj/libraryStudentProject/reader/ReaderController.java
@@ -1,9 +1,16 @@
 package pl.haladyj.libraryStudentProject.reader;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
+@RequestMapping("/readers")
 public class ReaderController {
 
     private final ReaderService readerService;
@@ -12,5 +19,34 @@ public class ReaderController {
     public ReaderController(ReaderService readerService) {
         this.readerService = readerService;
     }
+
+    @GetMapping
+    public List<ReaderDto> findAll(){
+        return readerService.findAllReaders();
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<ReaderDto> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(readerService.findReaderById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ReaderDto> createReader (@RequestBody @Validated ReaderDto readerDto){
+        readerDto = readerService.createReader(readerDto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(readerDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(readerDto);
+    }
+
+/*    @PutMapping("/update")
+    public ResponseEntity updateReader(@RequestParam("map") String updatedReader){
+
+
+    }*/
 
 }
