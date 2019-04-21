ef91d498345775e64f8efb7b9e0400bc56641acb
Pawel Haladyj <pawel@haladyj.pl>
2019-04-21 18:50:51 +0200

    BUG!!!

--- src/test/java/pl/haladyj/libraryStudentProject/reader/reader/ReaderServiceTest.java
+++ src/test/java/pl/haladyj/libraryStudentProject/reader/reader/ReaderServiceTest.java
@@ -11,12 +11,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import pl.haladyj.libraryStudentProject.reader.*;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.booleanThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -53,11 +56,33 @@ public class ReaderServiceTest {
 
     @Test
     public void findReaderById() {
+        when(readerRepository.findById(testReaderDto[0].getId())).thenReturn(Optional.of(testReader[0]));
+
+        ReaderDto readerDto = readerService.findReaderById(testReaderDto[0].getId());
+
+        assertEquals(testReader[0].getId(),readerDto.getId());
+        assertEquals(testReader[0].getSsn(),readerDto.getSsn());
+        assertEquals(testReader[0].getLastName(),readerDto.getLastName());
+        assertEquals(testReader[0].getFirstName(),readerDto.getFirstName());
+        assertEquals(testReader[0].getPhoneNumber(),readerDto.getPhoneNumber());
+    }
+
+    @Test(expected = ReaderNotFoundException.class)
+    public void doNotFindReaderByIdIfIdDoesNotExist() {
+        when(readerRepository.findById(Long.MAX_VALUE)).thenReturn(Optional.of(testReader[0]));
+
+        ReaderDto readerDto = readerService.findReaderById(testReaderDto[0].getId());
+
+        assertEquals(testReader[0].getId(),readerDto.getId());
+        assertEquals(testReader[0].getSsn(),readerDto.getSsn());
+        assertEquals(testReader[0].getLastName(),readerDto.getLastName());
+        assertEquals(testReader[0].getFirstName(),readerDto.getFirstName());
+        assertEquals(testReader[0].getPhoneNumber(),readerDto.getPhoneNumber());
     }
 
     @Test
     public void findAllReaders() {
-                Reader givenReader1 =testReader[0];
+        Reader givenReader1 =testReader[0];
         Reader givenReader2 =testReader[1];
         ReaderDto expectedReaderDto1 =testReaderDto[0];
         ReaderDto expectedReaderDto2 =testReaderDto[1];
@@ -72,8 +97,39 @@ public class ReaderServiceTest {
     }
 
     @Test
-    public void createReader() {
+    public void checkByIdIfReaderExists(){
+        Long notExistingReaderId = Long.MAX_VALUE;
+        Reader givenReader = testReader[0];
+
+        when(readerRepository.existsById(givenReader.getId())).thenReturn(true);
+
+        assertTrue(readerRepository.existsById(givenReader.getId()));
+        assertFalse(readerRepository.existsById(notExistingReaderId));
     }
+
+/*    @Test(expected = DuplicateReaderException.class)
+    public void createReaderThatAlreadyExists(){
+        Reader givenReader = testReader[0];
+        //ReaderDto duplicateGivenReader = readerConverter.toDto(givenReader);
+
+        List<Reader> expextedReaders = newArrayList(givenReader);
+        when(readerRepository.findAll()).thenReturn(expextedReaders);
+
+        ReaderDto duplicateGivenReader = readerService.createReader(readerConverter.toDto(givenReader));
+    }*/
+
+
+/*    @Test
+    public void createReader() {
+        Reader reader = new Reader(null, "Ian", "White", "12345678901","815253666");
+        Reader savedReader = testReader[0];
+        ReaderDto readerDto = new ReaderDto(null, "Ian", "White", "12345678901","815253666");
+        ReaderDto expectedReaderDto = testReaderDto[0];
+
+        ReaderDto savedReaderDto = readerService.createReader(readerDto);
+
+        assertEquals(savedReaderDto,expectedReaderDto);
+    }*/
 
     @Test
     public void updateReader() {
